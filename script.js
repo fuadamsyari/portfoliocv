ee23207fbe0b928a5ac8f67fefee3df29941c988
fuadamsyari <ahmadfuadamsyari@gmail.com>
2022-07-12 08:53:40 +0700

    menambahkan data

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 AOS.init();
 
-const tombol = document.querySelector('.tombol');
+let tombol = document.querySelector('.tombol');
 
 tombol.addEventListener('mouseover', function () {
   this.classList.add('btn-outline-danger');
   this.classList.remove('btn-secondary');
-});
-tombol.addEventListener('mouseout', function () {
-  this.classList.add('btn-secondary');
-  this.classList.remove('btn-outline-danger');
+  this.addEventListener('mouseout', function () {
+    this.classList.add('btn-secondary');
+    this.classList.remove('btn-outline-danger');
+  });
 });
